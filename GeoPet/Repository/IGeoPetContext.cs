5cdd24ff45a493ffd2abacccc3fb45b5d91e2fce
Sergio Francisco <spinheiro.lf@gmail.com>
2023-02-12 21:17:23 -0300

    Cria contextos

---
+++ GeoPet/Repository/IGeoPetContext.cs
@@ -0,0 +1,11 @@
+﻿namespace GeoPet.Repository;
+using Microsoft.EntityFrameworkCore;
+using GeoPet.Models;
+
+    public interface IGeoPetContext
+    {
+       public DbSet<Pets> Pets { get; set; }
+        public DbSet<LocationHistory> LocationHistory { get; set; }
+        public DbSet<CareGivers> CareGivers { get; set; }
+        public int SaveChanges();
+    }

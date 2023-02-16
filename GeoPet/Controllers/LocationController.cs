e90ca42a6c20cae39a18548b916b02c59c69be3f
Jorge Dorio <jorgegabrielbd.jj@gmail.com>
2023-02-16 03:02:16 -0300

    feat: location route

---
+++ GeoPet/Controllers/LocationController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GeoPet.Controllers
+{
+    [Route("location")]
+    [ApiController]
+    public class LocationController : ControllerBase
+    {
+        private readonly HttpClient _client;
+        private const string _baseUrl = "https://nominatim.openstreetmap.org/reverse?";
+        
+        public LocationController(HttpClient client)
+        {
+            _client = client;
+            _client.BaseAddress = new Uri(_baseUrl);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetLocation(string latitude, string longitude)
+        {
+            var response = await _client.GetAsync($"lat={latitude}&lon={longitude}&format=json");
+
+            if (response.IsSuccessStatusCode)
+            {
+                return Ok(response.Content.ReadFromJsonAsync<object>());
+            }
+
+            return BadRequest();
+        }
+    }
+}
